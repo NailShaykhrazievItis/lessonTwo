b4541f5c700d27e7975eb57a5541fea7a7c19b12
Almaz <Alzarg@yandex.ru>
2018-03-04 19:23:46 +0300

    fix + add character to menu

--- app/src/main/java/com/itis/android/lessontwo/ui/base/BaseActivity.java
+++ app/src/main/java/com/itis/android/lessontwo/ui/base/BaseActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import com.itis.android.lessontwo.R;
+import com.itis.android.lessontwo.ui.characterslist.CharactersListActivity;
 import com.itis.android.lessontwo.ui.comicslist.ComicsListActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -55,6 +56,9 @@ public abstract class BaseActivity extends AppCompatActivity {
                 case R.id.menu_comics:
                     Intent intent = new Intent(getApplicationContext(), ComicsListActivity.class);
                     startActivity(intent);
+                    break;
+                case R.id.menu_characters:
+                    CharactersListActivity.start(this);
                     break;
             }
             return true;

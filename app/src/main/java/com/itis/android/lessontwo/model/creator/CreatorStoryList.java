87bb90f3cddc939adffcf2684fcd2fa556af6331
Tilinina Alexandra <Sasha_til@mail.ru>
2018-03-05 17:39:26 +0300

    Added forgotten "public", fixed class name

--- app/src/main/java/com/itis/android/lessontwo/model/creator/CreatorStoryList.java
+++ app/src/main/java/com/itis/android/lessontwo/model/creator/CreatorStoryList.java
@@ -3,16 +3,16 @@ package com.itis.android.lessontwo.model.creator;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
-class CreatorStoryList {
+public class CreatorStoryList {
 
     @SerializedName("items")
-    private List<Story> stories;
+    private List<CreatorStory> stories;
 
-    public List<Story> getStories() {
+    public List<CreatorStory> getStories() {
         return stories;
     }
 
-    public void setStories(final List<Story> stories) {
+    public void setStories(final List<CreatorStory> stories) {
         this.stories = stories;
     }
 }

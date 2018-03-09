d3e6e04f225959489130b0cb3335494d43422651
mozilovich <mozilovich>
2018-03-09 21:42:19 +0300

    changed initComics()

--- app/src/main/java/com/itis/android/lessontwo/ui/comics/ComicsPresenter.java
+++ app/src/main/java/com/itis/android/lessontwo/ui/comics/ComicsPresenter.java
@@ -1,10 +1,6 @@
 package com.itis.android.lessontwo.ui.comics;
 
-import com.itis.android.lessontwo.api.ApiFactory;
-import com.itis.android.lessontwo.model.comics.ComicsResponse;
-import com.itis.android.lessontwo.model.comics.ComicsResponseData;
-import com.itis.android.lessontwo.ui.comics.ComicsContract;
-import com.itis.android.lessontwo.utils.RxUtils;
+import com.itis.android.lessontwo.repository.RepositoryProvider;
 
 /**
  * Created by User on 04.03.2018.
@@ -21,12 +17,8 @@ public class ComicsPresenter implements ComicsContract.Presenter {
 
     @Override
     public void initComics(long id) {
-        ApiFactory.getComicsService()
+        RepositoryProvider.provideComicsRepository()
                 .comics(id)
-                .map(ComicsResponse::getData)
-                .map(ComicsResponseData::getResults)
-                .map(list -> list.get(0))
-                .compose(RxUtils.async())
                 .subscribe(view::showComics, view::handleError);
     }
 }

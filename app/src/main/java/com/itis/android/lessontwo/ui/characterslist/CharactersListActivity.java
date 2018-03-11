1ef0b500d26ff278f12ec279510dc27a7b1adcd1
Ruslan <kavin91@mail.ru>
2018-03-11 22:48:48 +0300

    Merge branch 'valera071998' of https://github.com/RuslanMusin/lessonTwo
    # Conflicts:
    #	app/src/main/java/com/itis/android/lessontwo/ui/base/BaseActivity.java
    #	app/src/main/java/com/itis/android/lessontwo/ui/comics/ComicsActivity.java
    #	app/src/main/java/com/itis/android/lessontwo/ui/comics/ComicsPresenter.java
    #	app/src/main/java/com/itis/android/lessontwo/ui/comicslist/ComicsListPresenter.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/itis/android/lessontwo/ui/characterslist/CharactersListActivity.java
+++ app/src/main/java/com/itis/android/lessontwo/ui/characterslist/CharactersListActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;

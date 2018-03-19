d98a44852cb724861744bb1d191e49c53931a77d
Tony <tynx11@gmail.com>
2018-03-19 20:51:12 +0300

    Creators moved to MOXY

---
+++ app/src/main/java/com/itis/android/lessontwo/ui/creators/CreatorsView.java
@@ -0,0 +1,24 @@
+package com.itis.android.lessontwo.ui.creators;
+
+import com.arellomobile.mvp.MvpView;
+import com.itis.android.lessontwo.model.character.Character;
+import com.itis.android.lessontwo.model.creators.Creators;
+
+/**
+ * Created by Tony on 3/19/2018.
+ */
+
+public interface CreatorsView extends MvpView {
+
+    void setImage (Creators creators);
+
+    void getCreatorId();
+
+    void setName(Creators creators);
+
+    void handleError(Throwable error);
+
+
+
+
+}

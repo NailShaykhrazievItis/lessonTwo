4ab11a5b5bec14925b9b09be9ac74d9d73440f3b
Nail <shaykhrazievnail@gmail.com>
2018-11-25 21:14:39 +0300

    Update build tools version

--- app/src/main/java/com/itis/android/lessontwo/api/ComicsService.java
+++ app/src/main/java/com/itis/android/lessontwo/api/ComicsService.java
@@ -8,14 +8,12 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
-/**
- * Created by Nail Shaykhraziev on 25.02.2018.
- */
 
 public interface ComicsService {
 
     @GET("comics")
-    Single<ComicsResponse> comics(@Query("offset") Long offset, @Query("limit") Long limit,
+    Single<ComicsResponse> comics(@Query("offset") Long offset,
+                                  @Query("limit") Long limit,
                                   @Query("orderBy") String orderBy);
 
     @GET("comics/{comicsId}")
